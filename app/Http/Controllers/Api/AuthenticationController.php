d67c28d445497b3401d8b9d7f9b732ebaf30c3de
passionate-laravel-dev <taha.engineer26@gmail.com>
2025-02-25 18:03:35 +0200

    Implement Authentication using Laravel/Sanctum

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class AuthenticationController extends Controller
+{
+    public function signin(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|string'
+        ]);
+
+        try {
+            $user = User::where('email', $validated['email'])->first();
+
+            if (!$user || !Hash::check($validated['password'], $user->password)) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+
+            $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Signin successful!',
+                'token' => $token,
+            ]);
+        } catch (Throwable $th) {
+            Log::error('Signin happen issue: ' . $th->getMessage());
+            return response()->json([
+                'message' => 'Somethings went wrong!'
+            ], 500);
+        }
+    }
+
+    public function signup(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string'
+        ]);
+
+        try {
+            $validated['password'] = Hash::make($validated['password']);
+
+            User::create($validated);
+
+            return response()->json([
+                'message' => 'Signup successful!'
+            ]);
+        } catch (Throwable $th) {
+            Log::error('Signup happen issue: ' . $th->getMessage());
+            return response()->json([
+                'message' => 'Somethings went wrong!'
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
 use App\Http\Controllers\Api\SpotifyController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('spotify')->group(function () {
+Route::prefix('auth')->group(function () {
+    Route::post('signin', [AuthenticationController::class, 'signin']);
+    Route::post('signup', [AuthenticationController::class, 'signup']);
+})->withoutMiddleware('auth:sanctum');
+
+Route::prefix('spotify')->middleware('auth:sanctum')->group(function () {
     Route::get('search-items', [SpotifyController::class, 'searchItems']);
 
     Route::prefix('artists')->group(function () {
